663b5c5b1ae268183d804ebc13f4c549f956f24d
Rana-tringapps <96760690+Rana-tringapps@users.noreply.github.com>
2022-04-21 17:03:04 +0530

    Update stack.java

--- src/main/java/data_structure/stack.java
+++ src/main/java/data_structure/stack.java
@@ -5,7 +5,7 @@ public class stack
 {  
 public static void main(String[] args)   
 {  
-Deque<Integer> stk= new Stack<Integer>();  
+Vector<Integer> stk= new Vector<Integer>();  
 stk.push(1);  
 stk.push(2);  
 stk.push(3);  
